2a5400d81486703ae88d379769edcbee08a8d0ff
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-12-11 23:18:36 +0530

    Refactor

--- trainer/a3c/train.py
+++ trainer/a3c/train.py
@@ -22,249 +22,241 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
 def ensure_shared_grads(model, shared_model):
-    for param, shared_param in zip(model.parameters(),
-                                   shared_model.parameters()):
-        if shared_param.grad is not None:
-            return
-        shared_param._grad = param.grad
+	for param, shared_param in zip(model.parameters(),
+									shared_model.parameters()):
+		if shared_param.grad is not None:
+			return
+		shared_param._grad = param.grad
 
 
 def choose_action(model, state, hx, cx):
-    self.eval()
-    logits, _ = self.forward(s)
-    prob = F.softmax(logits, dim=1).data
-    m = self.distribution(prob)
-    return m.sample().numpy()[0]
+	self.eval()
+	logits, _ = self.forward(s)
+	prob = F.softmax(logits, dim=1).data
+	m = self.distribution(prob)
+	return m.sample().numpy()[0]
 
 def train(rank, args, shared_model, counter, lock, optimizer=None, select_sample=True):
-    torch.manual_seed(args.seed + rank)
+	torch.manual_seed(args.seed + rank)
 
-    print("Process No : {} | Sampling : {}".format(rank, select_sample))
+	print("Process No : {} | Sampling : {}".format(rank, select_sample))
 
-    FloatTensor = torch.cuda.FloatTensor if args.use_cuda else torch.FloatTensor
-    DoubleTensor = torch.cuda.DoubleTensor if args.use_cuda else torch.DoubleTensor
-    ByteTensor = torch.cuda.ByteTensor if args.use_cuda else torch.ByteTensor
+	FloatTensor = torch.cuda.FloatTensor if args.use_cuda else torch.FloatTensor
+	DoubleTensor = torch.cuda.DoubleTensor if args.use_cuda else torch.DoubleTensor
+	ByteTensor = torch.cuda.ByteTensor if args.use_cuda else torch.ByteTensor
 
 
-    env = gym_super_mario_bros.make(args.env_name)
-    # env = create_mario_env(args.env_name)
-    env.seed(args.seed + rank)
+	env = gym_super_mario_bros.make(args.env_name)
+	env.seed(args.seed + rank)
 
-    model = ActorCritic(env.observation_space.shape[0], len(ACTIONS))
-    if args.use_cuda:
-        model.cuda()
-    if optimizer is None:
-        optimizer = optim.Adam(shared_model.parameters(), lr=args.lr)
+	model = ActorCritic(env.observation_space.shape[0], len(ACTIONS))
+	if args.use_cuda:
+		model.cuda()
+	if optimizer is None:
+		optimizer = optim.Adam(shared_model.parameters(), lr=args.lr)
 
-    model.train()
+	model.train()
 
-    state = env.reset()
-    state = torch.from_numpy(np.flip(state,axis=0).copy())
-    # state = torch.from_numpy(state)
-    done = True
+	state = env.reset()
+	state = torch.from_numpy(np.flip(state,axis=0).copy())
+	done = True
 
-    episode_length = 0
-    for num_iter in count():
+	episode_length = 0
+	for num_iter in count():
 
-        if rank == 0:
-            # env.render()
+		if rank == 0:
 
-            if num_iter % args.save_interval == 0 and num_iter > 0:
-                print ("Saving model at :" + args.save_path)
-                torch.save(shared_model.state_dict(), args.save_path)
+			if num_iter % args.save_interval == 0 and num_iter > 0:
+				print ("Saving model at :" + args.save_path)
+				torch.save(shared_model.state_dict(), args.save_path)
 
-        if num_iter % (args.save_interval * 2.5) == 0 and num_iter > 0 and rank == 1:    # Second saver in-case first processes crashes
-            print ("Saving model for process 1 at :" + args.save_path)
-            torch.save(shared_model.state_dict(), args.save_path)
+		if num_iter % (args.save_interval * 2.5) == 0 and num_iter > 0 and rank == 1:    # Second saver in-case first processes crashes
+			print ("Saving model for process 1 at :" + args.save_path)
+			torch.save(shared_model.state_dict(), args.save_path)
 
-        # Sync with the shared model
-        model.load_state_dict(shared_model.state_dict())
-        if done:
-            cx = Variable(torch.zeros(1, 512)).type(FloatTensor)
-            hx = Variable(torch.zeros(1, 512)).type(FloatTensor)
-        else:
-            cx = Variable(cx.data).type(FloatTensor)
-            hx = Variable(hx.data).type(FloatTensor)
+		# Sync with the shared model
+		model.load_state_dict(shared_model.state_dict())
+		if done:
+			cx = Variable(torch.zeros(1, 512)).type(FloatTensor)
+			hx = Variable(torch.zeros(1, 512)).type(FloatTensor)
+		else:
+			cx = Variable(cx.data).type(FloatTensor)
+			hx = Variable(hx.data).type(FloatTensor)
 
-        values = []
-        log_probs = []
-        rewards = []
-        entropies = []
-        reason =''
+		values = []
+		log_probs = []
+		rewards = []
+		entropies = []
+		reason =''
 
-        for step in range(args.num_steps):
-            episode_length += 1
-            state_inp = Variable(state.unsqueeze(0)).type(FloatTensor)
-            value, logit, (hx, cx) = model((state_inp, (hx, cx)))
-            prob = F.softmax(logit, dim=-1)
-            log_prob = F.log_softmax(logit, dim=-1)
-            entropy = -(log_prob * prob).sum(-1, keepdim=True)
-            entropies.append(entropy)
+		for step in range(args.num_steps):
+			episode_length += 1
+			state_inp = Variable(state.unsqueeze(0)).type(FloatTensor)
+			value, logit, (hx, cx) = model((state_inp, (hx, cx)))
+			prob = F.softmax(logit, dim=-1)
+			log_prob = F.log_softmax(logit, dim=-1)
+			entropy = -(log_prob * prob).sum(-1, keepdim=True)
+			entropies.append(entropy)
 
-            if select_sample:
-                action = prob.multinomial(num_samples=1).data
-            else:
-                action = prob.max(-1, keepdim=True)[1].data
+			if select_sample:
+				action = prob.multinomial(num_samples=1).data
+			else:
+				action = prob.max(-1, keepdim=True)[1].data
 
-            log_prob = log_prob.gather(-1, Variable(action))
+			log_prob = log_prob.gather(-1, Variable(action))
 
-            action_out = ACTIONS[action]
+			action_out = ACTIONS[action]
+			env.render()
+			state, reward, done, _ = env.step(int(action))
 
-            # print("Process: {} Action: {}".format(rank, str(action)))
+			done = done or episode_length >= args.max_episode_length
+			reward = max(min(reward, 50), -50)
 
-            state, reward, done, _ = env.step(int(action))
+			with lock:
+				counter.value += 1
 
-            done = done or episode_length >= args.max_episode_length
-            reward = max(min(reward, 50), -50)
+			if done:
+				episode_length = 0
+				env.change_level(0)
+				state = env.reset()
+				print ("Process {} has completed.".format(rank))
 
-            with lock:
-                counter.value += 1
+			env.locked_levels = [False] + [True] * 31
+			state = torch.from_numpy(np.flip(state,axis=0).copy())
+			values.append(value)
+			log_probs.append(log_prob)
+			rewards.append(reward)
 
-            if done:
-                episode_length = 0
-                env.change_level(0)
-                state = env.reset()
-                print ("Process {} has completed.".format(rank))
+			if done:
+				break
 
-            env.locked_levels = [False] + [True] * 31
-            state = torch.from_numpy(np.flip(state,axis=0).copy())
-            # state = torch.from_numpy(state)
-            values.append(value)
-            log_probs.append(log_prob)
-            rewards.append(reward)
+		R = torch.zeros(1, 1)
+		if not done:
+			state_inp = Variable(state.unsqueeze(0)).type(FloatTensor)
+			value, _, _ = model((state_inp, (hx, cx)))
+			R = value.data
 
-            if done:
-                break
+		values.append(Variable(R).type(FloatTensor))
+		policy_loss = 0
+		value_loss = 0
+		R = Variable(R).type(FloatTensor)
+		gae = torch.zeros(1, 1).type(FloatTensor)
+		for i in reversed(range(len(rewards))):
+			R = args.gamma * R + rewards[i]
+			advantage = R - values[i]
+			value_loss = value_loss + 0.5 * advantage.pow(2)
 
-        R = torch.zeros(1, 1)
-        if not done:
-            state_inp = Variable(state.unsqueeze(0)).type(FloatTensor)
-            value, _, _ = model((state_inp, (hx, cx)))
-            R = value.data
+			# Generalized Advantage Estimataion
+			delta_t = rewards[i] + args.gamma * \
+				values[i + 1].data - values[i].data
+			gae = gae * args.gamma * args.tau + delta_t
 
-        values.append(Variable(R).type(FloatTensor))
-        policy_loss = 0
-        value_loss = 0
-        R = Variable(R).type(FloatTensor)
-        gae = torch.zeros(1, 1).type(FloatTensor)
-        for i in reversed(range(len(rewards))):
-            R = args.gamma * R + rewards[i]
-            advantage = R - values[i]
-            value_loss = value_loss + 0.5 * advantage.pow(2)
+			policy_loss = policy_loss - \
+				log_probs[i] * Variable(gae).type(FloatTensor) - args.entropy_coef * entropies[i]
 
-            # Generalized Advantage Estimataion
-            delta_t = rewards[i] + args.gamma * \
-                values[i + 1].data - values[i].data
-            gae = gae * args.gamma * args.tau + delta_t
+		total_loss = policy_loss + args.value_loss_coef * value_loss
 
-            policy_loss = policy_loss - \
-                log_probs[i] * Variable(gae).type(FloatTensor) - args.entropy_coef * entropies[i]
+		print ("Process {} - Reward: {} loss: ".format(rank, str(rewards)), total_loss.data)
+		optimizer.zero_grad()
 
-        total_loss = policy_loss + args.value_loss_coef * value_loss
+		(total_loss).backward()
+		torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
 
-        print ("Process {} loss :".format(rank), total_loss.data)
-        # print("Process: {} Episode: {}".format(rank,  str(episode_length)))
-        optimizer.zero_grad()
-
-        (total_loss).backward()
-        torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
-
-        ensure_shared_grads(model, shared_model)
-        optimizer.step()
-    print ("Process {} closed.".format(rank))
+		ensure_shared_grads(model, shared_model)
+		optimizer.step()
+	print ("Process {} closed.".format(rank))
 
 def test(rank, args, shared_model, counter):
-    torch.manual_seed(args.seed + rank)
+	torch.manual_seed(args.seed + rank)
 
-    FloatTensor = torch.cuda.FloatTensor if args.use_cuda else torch.FloatTensor
-    DoubleTensor = torch.cuda.DoubleTensor if args.use_cuda else torch.DoubleTensor
-    ByteTensor = torch.cuda.ByteTensor if args.use_cuda else torch.ByteTensor
+	FloatTensor = torch.cuda.FloatTensor if args.use_cuda else torch.FloatTensor
+	DoubleTensor = torch.cuda.DoubleTensor if args.use_cuda else torch.DoubleTensor
+	ByteTensor = torch.cuda.ByteTensor if args.use_cuda else torch.ByteTensor
 
-    env = gym_super_mario_bros.make(args.env_name)
-    # env = create_mario_env(args.env_name)
-    """
-        need to implement Monitor wrapper with env.change_level
-    """
-    # expt_dir = 'video'
-    # env = wrappers.Monitor(env, expt_dir, force=True, video_callable=lambda count: count % 10 == 0)
+	env = gym_super_mario_bros.make(args.env_name)
+	# env = create_mario_env(args.env_name)
+	"""
+		need to implement Monitor wrapper with env.change_level
+	"""
+	# expt_dir = 'video'
+	# env = wrappers.Monitor(env, expt_dir, force=True, video_callable=lambda count: count % 10 == 0)
 
-    env.seed(args.seed + rank)
+	env.seed(args.seed + rank)
 
-    model = ActorCritic(env.observation_space.shape[0], len(ACTIONS))
-    if args.use_cuda:
-        model.cuda()
-    model.eval()
+	model = ActorCritic(env.observation_space.shape[0], len(ACTIONS))
+	if args.use_cuda:
+		model.cuda()
+	model.eval()
 
-    state = env.reset()
-    state = torch.from_numpy(np.flip(state,axis=0).copy())
-    # state = torch.from_numpy(state)
-    reward_sum = 0
-    done = True
-    savefile = os.getcwd() + '/save/trained-models/mario_curves.csv'
+	state = env.reset()
+	state = torch.from_numpy(np.flip(state,axis=0).copy())
+	# state = torch.from_numpy(state)
+	reward_sum = 0
+	done = True
+	savefile = os.getcwd() + '/save/mario_curves.csv'
 
-    title = ['Time','No. Steps', 'Total Reward', 'Episode Length']
-    with open(savefile, 'a', newline='') as sfile:
-        writer = csv.writer(sfile)
-        writer.writerow(title)
+	title = ['Time','No. Steps', 'Total Reward', 'Episode Length']
+	with open(savefile, 'a', newline='') as sfile:
+		writer = csv.writer(sfile)
+		writer.writerow(title)
 
-    start_time = time.time()
+	start_time = time.time()
 
-    # a quick hack to prevent the agent from stucking
-    actions = deque(maxlen=4000)
-    episode_length = 0
-    with torch.no_grad():
-        while True:
-            episode_length += 1
-            ep_start_time = time.time()
-            # Sync with the shared model
-            if done:
-                model.load_state_dict(shared_model.state_dict())
-                cx = Variable(torch.zeros(1, 512)).type(FloatTensor)
-                hx = Variable(torch.zeros(1, 512)).type(FloatTensor)
+	# a quick hack to prevent the agent from stucking
+	actions = deque(maxlen=4000)
+	episode_length = 0
+	with torch.no_grad():
+		while True:
+			episode_length += 1
+			ep_start_time = time.time()
+			# Sync with the shared model
+			if done:
+				model.load_state_dict(shared_model.state_dict())
+				cx = Variable(torch.zeros(1, 512)).type(FloatTensor)
+				hx = Variable(torch.zeros(1, 512)).type(FloatTensor)
 
-            else:
-                cx = Variable(cx.data).type(FloatTensor)
-                hx = Variable(hx.data).type(FloatTensor)
+			else:
+				cx = Variable(cx.data).type(FloatTensor)
+				hx = Variable(hx.data).type(FloatTensor)
 
-            state_inp = Variable(state.unsqueeze(0)).type(FloatTensor)
-            value, logit, (hx, cx) = model((state_inp, (hx, cx)))
-            prob = F.softmax(logit, dim=-1)
-            action = prob.max(-1, keepdim=True)[1].data
+			state_inp = Variable(state.unsqueeze(0)).type(FloatTensor)
+			value, logit, (hx, cx) = model((state_inp, (hx, cx)))
+			prob = F.softmax(logit, dim=-1)
+			action = prob.max(-1, keepdim=True)[1].data
 
-            action_out = ACTIONS[action]
-            # print("Process: Test Action: {}".format(str(action_out)))
+			action_out = ACTIONS[action]
+			# print("Process: Test Action: {}".format(str(action_out)))
 
-            state, reward, done, _ = env.step(int(action))
-            # env.render()
-            done = done or episode_length >= args.max_episode_length
-            reward_sum += reward
+			state, reward, done, _ = env.step(int(action))
+			# env.render()
+			done = done or episode_length >= args.max_episode_length
+			reward_sum += reward
 
-            # a quick hack to prevent the agent from stucking
-            actions.append(action)
-            if actions.count(actions[0]) == actions.maxlen:
-                done = True
+			# a quick hack to prevent the agent from stucking
+			actions.append(action)
+			if actions.count(actions[0]) == actions.maxlen:
+				done = True
 
-            if done:
-                print("Time {}, num steps {}, FPS {:.0f}, episode reward {}, episode length {}".format(
-                    time.strftime("%Hh %Mm %Ss",
-                                time.gmtime(time.time() - start_time)),
-                    counter.value, counter.value / (time.time() - start_time),
-                    reward_sum, episode_length))
+			if done:
+				print("Time {}, num steps {}, FPS {:.0f}, episode reward {}, episode length {}".format(
+					time.strftime("%Hh %Mm %Ss",
+								time.gmtime(time.time() - start_time)),
+					counter.value, counter.value / (time.time() - start_time),
+					reward_sum, episode_length))
 
-                data = [time.time() - ep_start_time,
-                        counter.value, reward_sum, episode_length]
+				data = [time.time() - ep_start_time,
+						counter.value, reward_sum, episode_length]
 
-                with open(savefile, 'a', newline='') as sfile:
-                    writer = csv.writer(sfile)
-                    writer.writerows([data])
+				with open(savefile, 'a', newline='') as sfile:
+					writer = csv.writer(sfile)
+					writer.writerows([data])
 
-                reward_sum = 0
-                episode_length = 0
-                actions.clear()
-                time.sleep(60)
-                env.locked_levels = [False] + [True] * 31
-                env.change_level(0)
-                state = env.reset()
+				reward_sum = 0
+				episode_length = 0
+				actions.clear()
+				time.sleep(60)
+				env.locked_levels = [False] + [True] * 31
+				env.change_level(0)
+				state = env.reset()
 
-            state = torch.from_numpy(np.flip(state,axis=0).copy())
-            # state = torch.from_numpy(state)
+			state = torch.from_numpy(np.flip(state,axis=0).copy())
